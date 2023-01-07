f00f3bb3a8ce20a487600234e84ec10c1bf365a7
seftianhd <48037525+seftianhd@users.noreply.github.com>
2023-01-08 03:48:22 +0700

    add some function on BuyRepositoryTest

--- src/main/java/com/practice/ecommerce/entity/Goods.java
+++ src/main/java/com/practice/ecommerce/entity/Goods.java
@@ -32,6 +32,10 @@ public class Goods implements Serializable {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/com/practice/ecommerce/dao/BuyRepositoryTest.java
+++ src/test/java/com/practice/ecommerce/dao/BuyRepositoryTest.java
@@ -3,11 +3,13 @@ package com.practice.ecommerce.dao;
 import com.practice.ecommerce.entity.Buy;
 import com.practice.ecommerce.entity.BuyDetail;
 import com.practice.ecommerce.entity.Goods;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class BuyRepositoryTest {
@@ -15,9 +17,42 @@ public class BuyRepositoryTest {
     BuyRepository buyRepository;
     @Test
     void saveTest(){
-//        ArrayList<BuyDetail> buydetail = new ArrayList<>();
-//        buydetail.add(new BuyDetail(1,1,100,new Goods(),1));
-//        Buy buy = new Buy(1,1,1,1,buydetail);
-//        buyRepository.save(buy);
+        Goods good = getGood();
+
+        List<BuyDetail> buyDetails = new ArrayList<BuyDetail>();
+        buyDetails.add(getBuyDetail());
+        buyDetails.get(0).setGood(good);
+        buyDetails.get(0).setBuy(getBuy());
+
+        Buy buy = getBuy();
+        buy.setBuyDetails(buyDetails);
+        var verify = buyRepository.save(buy);
+        Assertions.assertNotNull(verify);
+    }
+
+    private Goods getGood(){
+        Goods good = new Goods();
+        good.setId(4);
+        good.setName("Sprite");
+        good.setBuyPrice(100);
+        good.setSellPrice(300);
+        good.setStock(10);
+        return good;
+    }
+
+    private BuyDetail getBuyDetail(){
+        BuyDetail buyDetail = new BuyDetail();
+        buyDetail.setPrice(1000);
+        buyDetail.setAmount(10);
+        return buyDetail;
+    }
+
+    private Buy getBuy(){
+        Buy buy = new Buy();
+        buy.setId(1);
+        buy.setDate(1);
+        buy.setTime(1);
+        buy.setSumOfPrice(100);
+        return buy;
     }
 }
