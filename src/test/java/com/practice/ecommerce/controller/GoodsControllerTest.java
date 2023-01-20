09f5435bd8705c4845c1cded238c9913ed9323de
seftianhd <seftianhd@gmail.com>
2023-01-21 04:22:26 +0700

    small update

--- src/test/java/com/practice/ecommerce/controller/GoodsControllerTest.java
+++ src/test/java/com/practice/ecommerce/controller/GoodsControllerTest.java
@@ -1,4 +1,12 @@
 package com.practice.ecommerce.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class GoodsControllerTest {
+    @Test
+    void testSomething(){
+
+    }
 }
